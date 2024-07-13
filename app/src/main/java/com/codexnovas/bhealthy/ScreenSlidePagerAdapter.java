81541be404a14f1d167b57f556722859158ec5a0
Sambit2005-lab <143246577+Sambit2005-lab@users.noreply.github.com>
2024-07-13 17:02:23 +0530

    slide functionality done

---
+++ app/src/main/java/com/codexnovas/bhealthy/ScreenSlidePagerAdapter.java
@@ -0,0 +1,34 @@
+package com.codexnovas.bhealthy;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ScreenSlidePagerAdapter extends FragmentStateAdapter {
+    public ScreenSlidePagerAdapter(@NonNull FragmentActivity fa) {
+        super(fa);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new welcome_to_bhealthy();
+            case 1:
+                return new smart_insights();
+            case 2:
+                return new understand_your_health();
+            case 3:
+                return new engage_with_people();
+            default:
+                return new welcome_to_bhealthy();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4; // Number of screens
+    }
+}
--- app/src/main/java/com/codexnovas/bhealthy/onboarding_background.java
+++ app/src/main/java/com/codexnovas/bhealthy/onboarding_background.java
@@ -1,24 +1,78 @@
 package com.codexnovas.bhealthy;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.os.Handler;
+import android.util.Log;
+import android.view.MotionEvent;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class onboarding_background extends AppCompatActivity {
+    private ViewPager2 viewPager;
+    private ScreenSlidePagerAdapter pagerAdapter;
+    private Handler sliderHandler = new Handler();
+    private boolean isUserInteracting = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_onboarding_background);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        viewPager = findViewById(R.id.viewPager);
+        pagerAdapter = new ScreenSlidePagerAdapter(this);
+        viewPager.setAdapter(pagerAdapter);
+
+        // Set a touch listener to handle user interaction
+        viewPager.setOnTouchListener((v, event) -> {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    isUserInteracting = true;
+                    sliderHandler.removeCallbacks(sliderRunnable);
+                    break;
+                case MotionEvent.ACTION_UP:
+                case MotionEvent.ACTION_CANCEL:
+                    isUserInteracting = false;
+                    sliderHandler.postDelayed(sliderRunnable, 6000);
+                    break;
+            }
+            return false;
         });
+
+        // Start the slider handler
+        sliderHandler.postDelayed(sliderRunnable, 6000); // 3 seconds delay
+    }
+
+    private Runnable sliderRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (!isUserInteracting) {
+                int nextItem = viewPager.getCurrentItem() + 1;
+                if (nextItem < pagerAdapter.getItemCount()) {
+                    viewPager.setCurrentItem(nextItem);
+                    Log.d("SliderRunnable", "Sliding to item: " + nextItem);
+                } else {
+                    // Stop sliding on the last page
+                    Log.d("SliderRunnable", "Reached the last item, stopping slide");
+                    sliderHandler.removeCallbacks(sliderRunnable);
+                    return;
+                }
+                sliderHandler.postDelayed(this, 6000); // 3 seconds delay
+            }
+        }
+    };
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sliderHandler.removeCallbacks(sliderRunnable);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (!isUserInteracting) {
+            sliderHandler.postDelayed(sliderRunnable, 3000); // Resume with 3 seconds delay
+        }
     }
 }
+
