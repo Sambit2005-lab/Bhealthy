069a7ef5c52ccafe10ffca9a79241e363960e21a
Sambit2005-lab <143246577+Sambit2005-lab@users.noreply.github.com>
2024-07-13 15:26:55 +0530

    Lottie animation added and functionality set up initiated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation ("com.airbnb.android:lottie:6.4.1")
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
