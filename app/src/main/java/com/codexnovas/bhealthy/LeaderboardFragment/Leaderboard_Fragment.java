476cb67e023111503d44a632c748f4d1040d62bd
Sambit2005-lab <143246577+Sambit2005-lab@users.noreply.github.com>
2024-07-17 18:51:26 +0530

    package added

--- app/src/main/java/com/codexnovas/bhealthy/LeaderboardFragment/Leaderboard_Fragment.java
+++ app/src/main/java/com/codexnovas/bhealthy/LeaderboardFragment/Leaderboard_Fragment.java
@@ -1,4 +1,4 @@
-package com.codexnovas.bhealthy;
+package com.codexnovas.bhealthy.LeaderboardFragment;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.codexnovas.bhealthy.R;
 
 /**
  * A simple {@link Fragment} subclass.
